440febb91980b00440e72840cc3b1d94f662b6ba
igorkhisamiev75 <khisamievigor@gmail.com>
2021-08-26 17:27:53 +0300

    Кнопочка добавлена, нужно добавить связь с линковоной модели

---
+++ BIMv2Buttons/App.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Windows.Media.Imaging;
+
+namespace BIMv2Buttons
+{
+    public class App : IExternalApplication
+    {
+        // define a method that will create our tab and button
+        static void AddRibbonPanel(UIControlledApplication application)
+        {
+
+            String tabName = "BIMv2";    // создание вкладки
+            application.CreateRibbonTab(tabName);
+
+            // Add a new ribbon panel
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel(tabName, "Надстройки MRGT v1.0"); //подпись под кнопками
+
+            // Get dll assembly path
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+
+            // create push button for CurveTotalLength  //namespace и класс
+            PushButtonData b1Data = new PushButtonData("cmdWriteNameRoomInPlumbingFixtures", "Записать" + Environment.NewLine + "в элементы", thisAssemblyPath, "BIMv2.WriteNameRoomInPlumbingFixtures");
+
+            PushButton pb1 = ribbonPanel.AddItem(b1Data) as PushButton;
+            pb1.ToolTip = "Transfer number and name to elements";
+
+            BitmapImage pb1Image = new BitmapImage(new Uri("pack://application:,,,/BIMv2Buttons;component/Resources/transferNameRoom.png"));
+            pb1.LargeImage = pb1Image;
+            
+            //попробуем создать еще одну кнопку
+            ribbonPanel.AddSeparator(); //разделитель между кнопками
+            
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            // do nothing
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            // call our method that will load up our toolbar
+            AddRibbonPanel(application);
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ BIMv2Buttons/WriteNameRoom/WriteNameRoomInPlumbingFixtures.cs
@@ -0,0 +1,120 @@
+﻿#region Namespace
+using System;
+using System.Collections.Generic;
+
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+#endregion
+
+namespace BIMv2
+{
+    [Transaction(TransactionMode.Manual)]
+    class WriteNameRoomInPlumbingFixtures: IExternalCommand
+    {
+        Application _app;
+        Document _doc;
+        private Autodesk.Revit.Creation.Application creApp;
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            UIApplication uiApp = commandData.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            _app = uiApp.Application;
+            _doc = uiDoc.Document;
+            creApp = commandData.Application.Application.Create;
+            
+            using (Transaction t = new Transaction(_doc))
+            {
+                t.Start("Select all plumbing fixtures");
+                WriteNameRoomInElementMethod();
+
+                TaskDialog.Show("У тебя получилось", "Ты красавчик!☺☺☺");
+                t.Commit();
+
+                t.Start();
+
+                var res = t.Commit();
+
+            }
+
+            return Result.Succeeded;
+        }
+
+        private void WriteNameRoomInElementMethod()
+        {
+            string nameRoom = "";
+
+            IList<Element> allElements = GetAllElements();
+
+            for (int i = 0; i < allElements.Count; i++)
+            {
+               Location loc = allElements[i].Location;
+               
+               LocationPoint locPoint = (LocationPoint)loc;
+               XYZ pointPF = locPoint.Point;
+
+               Room myRoom = _doc.GetRoomAtPoint(pointPF);
+
+               if (myRoom == null)
+               { 
+                   nameRoom = "It's not in Room";
+                   allElements[i].get_Parameter(new Guid("c78f0a7d-b68b-4d21-a247-1c8c6ced8bc5"))
+                       .Set(nameRoom).ToString();
+                   
+               }
+               else
+               {
+                   nameRoom = myRoom.get_Parameter(BuiltInParameter.ROOM_NUMBER).AsString() + " " +
+                              myRoom.get_Parameter(BuiltInParameter.ROOM_NAME).AsString();
+                   
+                   allElements[i].get_Parameter(new Guid("c78f0a7d-b68b-4d21-a247-1c8c6ced8bc5"))
+                       .Set(nameRoom).ToString();
+                }
+            }
+            
+        }
+
+        public IList<Element> GetAllElements()
+        {
+            // List all plumbing fixtures
+            var plumbingFixCollector = new FilteredElementCollector(_doc).OfClass(typeof(FamilyInstance));
+            plumbingFixCollector.OfCategory(BuiltInCategory.OST_PlumbingFixtures);
+            IList<Element> plFixList = plumbingFixCollector.ToElements();
+
+            // List all generic
+            var genericCollector = new FilteredElementCollector(_doc).OfClass(typeof(FamilyInstance));
+            genericCollector.OfCategory(BuiltInCategory.OST_GenericModel);
+            IList<Element> plFixList2 = genericCollector.ToElements();
+
+            foreach (var VARIABLE in plFixList2)
+            {
+                plFixList.Add(VARIABLE);
+            }
+
+            // List all SE
+            var seCollector = new FilteredElementCollector(_doc).OfClass(typeof(FamilyInstance));
+            seCollector.OfCategory(BuiltInCategory.OST_SpecialityEquipment);
+            IList<Element> plFixList3 = seCollector.ToElements();
+
+            foreach (var VARIABLE in plFixList3)
+            {
+                plFixList.Add(VARIABLE);
+            }
+
+            // List all furniture
+            var fCollector = new FilteredElementCollector(_doc).OfClass(typeof(FamilyInstance));
+            fCollector.OfCategory(BuiltInCategory.OST_Furniture);
+            IList<Element> plFixList4 = fCollector.ToElements();
+
+            foreach (var VARIABLE in plFixList4)
+            {
+                plFixList.Add(VARIABLE);
+            }
+            
+            return plFixList;
+        }
+    }
+}

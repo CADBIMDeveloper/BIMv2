6fda35ab1e3992c8a6b0d90a2564a64d08e76d86
igorkhisamiev75 <khisamievigor@gmail.com>
2021-08-26 10:27:34 +0300

    Добавьте файлы проекта.

---
+++ BIMv2/RoomNumberToElements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+namespace BIMv2
+{
+    [Transaction(TransactionMode.Manual)]
+    public class RoomNumberToElements
+    {
+    }
+}
